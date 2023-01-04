e8277ba335bf8f12582dbad152dfa7e5e492cc08
Lamisa Saadat <ls.lamisa12@gmail.com>
2023-01-04 12:46:25 -0500

    Created a project input form to add project by creating a class and instantiating it

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,28 @@
-// Code goes here!
+class ProjecInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    //render a form that belongs in the instance
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+    this.attach();
+  }
+
+  private attach() {
+    //insert an HTML element
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjecInput();
