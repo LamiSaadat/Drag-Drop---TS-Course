15c1982c794ecdbca7b855dd34be9a51bc344b52
Lamisa Saadat <ls.lamisa12@gmail.com>
2023-01-04 19:44:50 -0500

    Used a getter method to transform input data to show differently in different situations

--- src/app.ts
+++ src/app.ts
@@ -328,6 +328,12 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
 
+  get persons() {
+    return this.project.people === 1
+      ? "1 person assigned"
+      : `${this.project.people} persons assigned`;
+  }
+
   //the hostElementId and the newElementId will be different for each item so we are getting them from the constructor and passing it into the super
   constructor(hostId: string, project: Project) {
     super("single-project", hostId, false, project.id);
@@ -341,8 +347,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
   renderContent() {
     this.element.querySelector("h2")!.textContent = this.project.title;
-    this.element.querySelector("h3")!.textContent =
-      this.project.people.toString();
+    this.element.querySelector("h3")!.textContent = this.persons;
     this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
